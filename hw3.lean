587accbcd9f88935469e771850513f7b3aea6aa4
Dowland Aiello <dowlandaiello@gmail.com>
2025-04-20 10:44:27 -0700

    Hw3 done.

--- hw3.lean
+++ hw3.lean
@@ -24,7 +24,6 @@ example : limit (fun x : ℝ => x) a a
     simp_all
     exact lt_trans h_x (by simp_all)
   ⟩⟩
-
 --Problem 3: lim_{x → 0} |x| = 0
 example : limit (fun x : ℝ => |x|) 0 0
   | ε, hε => ⟨ε / 2, ⟨by simp_all, fun x h_x => by
@@ -68,19 +67,71 @@ theorem notContinous : ¬(isContinuous step 0)
 
 --Let's now define the derivative of a function f at a ∈ ℝ
 def derivative (f : ℝ → ℝ) (a : ℝ) (D : ℝ) : Prop :=
-  isContinuous (fun x : ℝ => if x = a then D else (f x - f a) / (x - a)) a
+  isContinuous (λx : ℝ => if x = a then D else (f x - f a) / (x - a)) a
 
 --Problem 7: If f(x) = 3, then f'(a) = 0 for all a
 def f₇ (x : ℝ) : ℝ := 3
-example : derivative f₇ a 0 := by
-  sorry
+example : derivative f₇ a 0
+  | ε, h_ε => ⟨1, ⟨by simp, λx h_x => by
+    simp_all
+    split_ifs
+    case pos h =>
+      simp_all
+    case neg h =>
+      rw [abs_div]
+      unfold f₇
+      simp
+      exact h_ε
+  ⟩⟩
 
 --Problem 8: If f(x) = x, then f'(a) = 1 for all a
+-- Note, this was a typo. 0 Replaced with one here.
 def f₈ (x : ℝ) : ℝ := x
-example : derivative f₈ a 0 := by
-  sorry
+example : derivative f₈ a 1
+  | ε, h_ε => ⟨ε, ⟨h_ε, λx h_x => by
+    simp_all
+    split_ifs
+    case pos h =>
+      simp_all
+    case neg h =>
+      unfold f₈
+      rw [div_self, sub_self, abs_zero]
+      exact h_ε
+      exact (sub_ne_zero.mpr h)
+  ⟩⟩
 
 --Bonus problem: derivative of x^2 is 2x
 def square (x : ℝ) : ℝ := x^2
-example : (x : ℝ) → derivative square x (2 * x) := by
-  sorry
+example : (x : ℝ) → derivative square x (2 * x)
+  | x, ε, h_ε => ⟨ε, ⟨by simp_all, λx₂ h₂_x => by
+    simp_all
+    split_ifs
+    case pos h =>
+      simp_all
+    case neg h =>
+      unfold square
+      ring_nf
+      repeat rw [sub_eq_add_neg]
+      rw [← add_assoc]
+      repeat rw [neg_mul_eq_neg_mul]
+      calc
+        |x₂ ^ 2 * (x₂ + -x)⁻¹ + -x * 2 + -x ^ 2 * (x₂ + -x)⁻¹| = |x₂ ^ 2 * (x₂ + -x)⁻¹ + -x ^ 2 * (x₂ + -x)⁻¹ + -x * 2| := by
+          ring_nf
+        |x₂ ^ 2 * (x₂ + -x)⁻¹ + -x ^ 2 * (x₂ + -x)⁻¹ + -x * 2| = |(x₂ + -x)⁻¹ * x₂ ^ 2 + (x₂ + -x)⁻¹ * -x ^ 2 + -x * 2| := by
+          ring_nf
+        |(x₂ + -x)⁻¹ * x₂ ^ 2 +  (x₂ + -x)⁻¹ * -x ^ 2 + -x * 2| = |(x₂ + -x)⁻¹ * (x₂ ^ 2 + -x ^ 2) + -x * 2| := by
+          ring_nf
+        |(x₂ + -x)⁻¹ * (x₂ ^ 2 + -x ^ 2) + -x * 2| = |(x₂ + -x)⁻¹ * (x₂ + x) * (x₂ - x) + -x * 2| := by
+          ring_nf
+        |(x₂ + -x)⁻¹ * (x₂ + x) * (x₂ - x) + -x * 2| = |(x₂ - x)⁻¹ * (x₂ + x) * (x₂ - x) + -x * 2| := by
+          rw [← sub_eq_add_neg]
+        |(x₂ - x)⁻¹ * (x₂ + x) * (x₂ - x) + -x * 2| = |(x₂ + x) * ((x₂ - x) * (x₂ - x)⁻¹) + -x * 2| := by
+          ring_nf
+        |(x₂ + x) * ((x₂ - x) * (x₂ - x)⁻¹) + -x * 2| = |(x₂ + x) * 1 + -x * 2| := by
+          rw [mul_inv_cancel₀ $ sub_ne_zero.mpr h]
+        |(x₂ + x) * 1 + -x * 2| = |x₂ + x + -x * 2| := by
+          ring_nf
+        |x₂ + x + -x * 2| = |x₂ - x| := by
+          ring_nf
+        |x₂ - x| < ε := h₂_x
+  ⟩⟩
